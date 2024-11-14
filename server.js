bb119ba86e85256da74cec2a9452de43c991b346
DeviPothula <devikapothula597@gmail.com>
2024-11-14 18:55:33 +0530

    my first comit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.get('/', (req, res) => {
+  res.send('Hi devi this is your node js');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
